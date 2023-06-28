884e2593476ee1277267264e0e96a34627cfba99
carloshtbraga <carloshtbraga@gmail.com>
2023-06-28 11:15:25 -0300

    :bug:

--- tests/test_average2 copy.py
+++
@@ -1,9 +0,0 @@
-from src.average import find_average
-
-
-def test_average():
-    assert find_average([1, 1, 1]) == 1
-    assert find_average([7.5, 8, 8.5]) == 8
-    assert find_average([0, 10, 0]) == 10/3
-    assert find_average([10, 10, 10]) == 10
-    assert find_average([40, 50, 30]) == (40 + 50 + 30) / 3
