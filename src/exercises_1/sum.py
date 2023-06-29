6526bd42c14b9aeb943ead59829542ee92f22c99
carloshtbraga <carloshtbraga@gmail.com>
2023-06-28 22:52:43 -0300

    oi

--- src/exercises_1/sum.py
+++ src/exercises_1/sum.py
@@ -1,5 +1,5 @@
 # Somar dois números:
-# Implemente uma função que recebe dois números como
+# Impldemente uma função que recebe dois números como
 # entrada e retorna a soma deles.
 
 def sum(number, other_number):
