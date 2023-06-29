3b2972e3d08e42cb0ab6e084e1955f82bd6279f8
carloshtbraga <carloshtbraga@gmail.com>
2023-06-28 22:40:59 -0300

    :bug:

--- src/exercises_1/average.py
+++ src/exercises_1/average.py
@@ -1,7 +1,7 @@
 # Calcular a média de uma lista de números:
 # Implemente uma função que recebe uma lista de números
 # como entrada e calcula a média aritmética dos valores
-# presentes na lista. A função deve retornar a média.
+# presentess na lista. A função deve retornar a média.
 
 
 def average(list_of_numbers):
