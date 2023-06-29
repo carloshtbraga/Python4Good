a118a2544fb2a950b428835f9394e16251691e6b
carloshtbraga <carloshtbraga@gmail.com>
2023-06-28 23:06:46 -0300

    :bug:

--- src/exercises_2/sum_digits.py
+++ src/exercises_2/sum_digits.py
@@ -1,6 +1,6 @@
 # Somar os dígitos de um número inteiro:
 # Escreva uma função que recebe um número inteiro como entrada e calcula
-# a soma de todos os seus dígitos. A função deve retornar a soma.
+# a soma de todos os seus dígitos. A função deve retornar a somaaa.
 
 def sum_digits(number):
     ...
